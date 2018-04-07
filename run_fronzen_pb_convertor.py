4ea91d2950598ff72be496fb4e183fba8699256e
jaewookkang <jwkang@soundl.ly>
2018-04-07 19:36:19 +0900

    The first commit

---
+++ run_fronzen_pb_convertor.py
@@ -0,0 +1,41 @@
+#-*- coding: utf-8 -*-
+#! /usr/bin/env python
+'''
+    filename: run_fronzen_pb_convertor.py
+
+    description:
+
+        -  To generate frozen graph file (.pb) by combining graphdef file (.pb) and checkpoint (.ckpt)
+
+    references:
+        - https://github.com/tensorflow/tensorflow/blob/master/tensorflow/docs_src/mobile/tflite/devguide.md#2-convert-the-model-format
+        - https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/lite/toco/g3doc/cmdline_examples.md#savedmodel
+
+    author: Jaewook Kang
+    date  : 2018 Apr
+'''
+
+import sys
+from os import getcwd
+
+input_model_path    = getcwd()+'/pb_and_ckpt/lenet5/'
+output_model_path   = getcwd()+'/pb_and_ckpt/lenet5/frozen_pb_out/'
+
+# The output node name is from Tensorboard
+output_node_names   = 'model_out/Softmax'
+
+sys.path.insert(0,  input_model_path)
+sys.path.insert(0,  getcwd()+'/convertor/')
+
+from tflite_convertor import TFliteConvertor
+
+
+tflite_convertor = TFliteConvertor()
+
+tflite_convertor.set_config_for_frozen_graph(input_dir_path=input_model_path+'runtrain-20180405161551/',
+                                             input_pb_name='tf_graph_def_lenet5.pb',
+                                             input_ckpt_name='lenet5_model_variable.ckpt',
+                                             output_dir_path=output_model_path,
+                                             output_node_names=output_node_names)
+
+tflite_convertor.convert_to_frozen_graph()
---
+++ run_tflite_convertor.py
@@ -0,0 +1,58 @@
+#-*- coding: utf-8 -*-
+#! /usr/bin/env python
+'''
+    filename: run_tflte_convertor.py
+
+    description:
+
+        -  To convert tensorflow frozen graph to  tflite format
+
+    references:
+        - https://github.com/tensorflow/tensorflow/blob/master/tensorflow/docs_src/mobile/tflite/devguide.md#2-convert-the-model-format
+        - https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/lite/toco/g3doc/cmdline_examples.md#savedmodel
+
+    author: Jaewook Kang
+    date  : 2018 Apr
+'''
+
+import sys
+from os import getcwd
+sys.path.insert(0,  getcwd()+'/convertor/')
+from tflite_convertor import TFliteConvertor
+
+# your frozen graph pb
+input_frozen_pb_path    = getcwd()+'/pb_and_ckpt/lenet5/frozen_pb_out/'
+sys.path.insert(0,  input_frozen_pb_path)
+
+# your dir for exporting tflite file
+output_tflite_path   = getcwd()+'/pb_and_ckpt/lenet5/tflite_out/'
+
+
+# your dir path for tensorflow source
+PATH_TENSORFLOW_SRC = '/Users/jwkangmacpro/SourceCodes/tensorflow/'
+
+
+# The output/input node names are obtained from Tensorboard
+output_node_names   = 'model_out/Softmax'
+input_node_names    =  'input'
+
+# input placeholder shape
+input_shape_str = '1,28,28,1'
+
+
+
+tflite_convertor = TFliteConvertor()
+
+# tflite config
+tflite_convertor.set_config_for_tflite(input_dir_path   =input_frozen_pb_path,
+                                    output_dir_path     =output_tflite_path,
+                                    input_pb_file       ='frozen_tf_graph_def_lenet5.pb',
+                                    output_tflite_file  ='tflite_lenet5.tflite',
+                                    inference_type      ='FLOAT',
+                                    input_shape         = input_shape_str,
+                                    input_array         = input_node_names,
+                                    output_array        = output_node_names,
+                                    tf_src_dir_path     = PATH_TENSORFLOW_SRC)
+
+# frozen grpah to tflite conversion
+tflite_convertor.convert_to_tflite_from_frozen_graph()
